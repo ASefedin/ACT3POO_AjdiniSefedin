0c8c29e52bfae6e45c2530c95b2e88edb3e156d8
ASefedin <115707837+ASefedin@users.noreply.github.com>
2023-11-16 23:09:48 +0100

    Add files via upload

---
+++ ACT3_AjdiniSefedinElephant/Act3Elephant/Act3Elephant/Elephant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act3Elephant
+{
+    internal class Elephant
+    {
+        private string _nom;
+        private uint _tailleOreille;
+
+        public uint TailleOreilles
+        {
+            get { return _tailleOreille; }
+            set { _tailleOreille = value; }
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+        }
+        public Elephant(string nom, uint tailleOreille)
+        {
+            _nom = nom;
+            _tailleOreille = tailleOreille;
+        }
+        public string AfficheQuiJeSuis()
+        {
+            return "la taille des oreilles de " + Nom + " est de : " + TailleOreilles;
+        }
+        public void EcouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+
+    }
+}
---
+++ ACT3_AjdiniSefedinElephant/Act3Elephant/Act3Elephant/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Act3Elephant;
+
+string reset = "";
+Elephant E1 = new Elephant("Zazou", 82);
+Elephant E2 = new Elephant("Titi", 100);
+
+do
+{
+    Console.WriteLine("Bienvenue dans cet échange d'éléphants");
+    Console.WriteLine("Tapez : ");
+    Console.WriteLine("1 : pour afficher les informations de Zazou");
+    Console.WriteLine("2 : pour Titi");
+    Console.WriteLine("3 : pour les échanger");
+    Console.WriteLine("4 : pour voir le transfert d'un message de Zazou à Titi");
+    Console.WriteLine("5 : pour réviser la notion de tableaux et l'appliquer à des objets");
+    string reponse = Console.ReadLine();
+    switch (reponse)
+    {
+        case "1":
+            Console.WriteLine(E1.AfficheQuiJeSuis());
+            break;
+        case "2":
+            Console.WriteLine(E2.AfficheQuiJeSuis());
+            break;
+        case "3":
+            Elephant tempo = E1;
+            E1 = E2;
+            E2 = tempo;
+
+            break;
+        case "4":
+            E1.EnvoieMessage("Hello Titi !", E2);
+            break;
+        case "5":
+            Elephant[] tabElephant = new Elephant[7];
+            tabElephant[0] = E1;
+            tabElephant[1] = E2;
+            tabElephant[2] = new Elephant("Quelqu'un", 10);
+            Elephant Elephant = tabElephant[0];
+            for (int i = 0; i < tabElephant.Length; i++)
+            {
+                if (tabElephant[i].TailleOreilles > Elephant.TailleOreilles)
+                {
+                    Elephant = tabElephant[i];
+                }
+
+            }
+            Console.WriteLine(Elephant.Nom + " a les bien grosse oreilles d'une taille de : " + Elephant.TailleOreilles);
+            break;
+    } while (reset == " ") ;
